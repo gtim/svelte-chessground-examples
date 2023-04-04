f338d9a4c49bf3ee55571e39d2cfddc3dcb8d7ba
Tim N <tim@gurka.se>
2023-04-04 22:45:00 +0200

    static adapter for github-pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import '../app.css';
 	import {title} from '$lib/stores.js';
+	import { base } from '$app/paths';
 </script>
 
 
@@ -8,11 +9,11 @@
 <div class="container">
 	<div class="sidebar">
 		<ul>
-			<li><a href="/">Default configuration</a></li>
-			<li><a href="/fen">Custom FEN</a></li>
-			<li><a href="/legal">Only legal moves</a></li>
-			<li><a href="/vs-ai">Play vs. random AI</a></li>
-			<li><a href="/watch-ai">Watch random AIs play</a></li>
+			<li><a href="{base}/">Default configuration</a></li>
+			<li><a href="{base}/fen">Custom FEN</a></li>
+			<li><a href="{base}/legal">Only legal moves</a></li>
+			<li><a href="{base}/vs-ai">Play vs. random AI</a></li>
+			<li><a href="{base}/watch-ai">Watch random AIs play</a></li>
 		</ul>
 	</div>
 	<div class="example">
