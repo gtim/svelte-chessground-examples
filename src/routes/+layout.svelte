3ff6fb7d903bc0e95a0108bbc9dced46d6715628
Tim N <tim@gurka.se>
2023-04-04 14:41:57 +0200

    examples: basic and FEN+orientation

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<h1>svelte-chessground examples</h1>
+
+<ul>
+<li><a href="/">Default configuration</a></li>
+<li><a href="/fen">Custom FEN on flipped board</a></li>
+<li>Play legal moves</li>
+<li>Play vs random AI</li>
+</ul>
+
+<slot/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import {Chessground} from 'svelte-chessground';
+</script>
+
+<div style="width:512px;height:512px;">
+	<Chessground />
+</div>
