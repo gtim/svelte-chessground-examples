9a3dfa4e9613a63c70a4b229d3ec902bd96d65f4
Tim N <tim@gurka.se>
2023-04-06 10:15:11 +0200

    0.0.5: fen/orientation props

--- src/routes/fen/+page.svelte
+++ src/routes/fen/+page.svelte
@@ -3,12 +3,8 @@
 
 	import {title} from '$lib/stores.js';
 	title.set('Custom FEN');
-
-	let config = {
-		fen: 'rnb1k1nr/p2p1ppp/3B4/1pbN1N1P/4P1P1/3P1Q2/PqP5/R4KR1 b kq - 1 18',
-		orientation: 'black'
-	};
 </script>
 
-<Chessground {config}/>
-
+<Chessground
+	orientation='black'
+	fen='r3k1nr/p2p1ppp/3B4/1pbN1N1P/4P1P1/3P1Q2/PqP5/R4KR1 b kq - 1 18'/>
